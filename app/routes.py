75983c1ffd5840dc994064f57a92f80bfa9b80be
Your Name <you@example.com>
2022-05-16 18:04:40 +0800

    added way to access the target no. vi jinja and python script

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
+
+from importlib import import_module
 from flask import render_template
 from app import app
+from app import python_scripts
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("skeleton.html")
+    
+    target= python_scripts.dummy.py
+    return render_template("skeleton.html",target=target)
