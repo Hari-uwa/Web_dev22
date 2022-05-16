2bca01665777b720a65f783ac16ae76204a7aa65
Jennifer Nguyen <thitrangngannguyen@gmail.com>
2022-05-16 14:35:53 +0800

    Pop Up

--- front.js
+++ front.js
@@ -25,16 +25,16 @@ const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
-button.addEventListener('click', () => {
+  button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget)
     openModal(modal)
 })
 })
 
 overlay.addEventListener('click', () => {
-const modals = document.querySelectorAll('.modal.active')
-modals.forEach(modal => {
-    closeModal(modal)
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+      closeModal(modal)
 })
 })
 
@@ -46,13 +46,13 @@ button.addEventListener('click', () => {
 })
 
 function openModal(modal) {
-if (modal == null) return
-modal.classList.add('active')
-overlay.classList.add('active')
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
 }
 
 function closeModal(modal) {
-if (modal == null) return
-modal.classList.remove('active')
-overlay.classList.remove('active')
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
 }
