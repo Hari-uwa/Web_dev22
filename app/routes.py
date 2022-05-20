7635f4563c2d4498aa35e45c38fc4bc91967ebd5
Your Name <you@example.com>
2022-05-20 15:24:28 +0800

    db set up packages

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,11 @@
 from importlib import import_module
 from flask import render_template
 from app import app
-from app import dummy
+from app import eqn_gen
 
 @app.route('/')
 @app.route('/index')
 def index():
     
-    target= dummy.math()[1][-1]
+    target= eqn_gen.math()[1][-1]
     return render_template("skeleton.html",target=target)
