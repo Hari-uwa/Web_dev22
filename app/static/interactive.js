1911fef175bac611841a80f22c8cf700b915dd3b
Jennifer Nguyen <thitrangngannguyen@gmail.com>
2022-05-22 21:38:44 +0800

    Instruction Modal

--- app/static/interactive.js
+++ app/static/interactive.js
@@ -1,11 +1,17 @@
+//Modal Buttons on Front Page
 $(document).ready(function() {
     const settingsButton = document.getElementById("settingsButton");
     const statsButton = document.getElementById("statsButton");
+    const howtoButton = document.getElementById("howtoButton");
+
     settingsButton.addEventListener('click', () => {
         $('#settingsModal').modal('show');
     });
     statsButton.addEventListener('click', () => {
         $('#statsModal').modal('show');
+    });
+    howtoButton.addEventListener('click', () => {
+        $('#howtoModal').modal('show');
     });
 });
 
