0f6667a6c1392952921c9e6546dc5828a22df451
Jennifer Nguyen <thitrangngannguyen@gmail.com>
2022-05-23 01:49:33 +0800

    Dark theme testing

--- app/static/interactive.js
+++ app/static/interactive.js
@@ -1,5 +1,6 @@
 //Modal Buttons on Front Page
 $(document).ready(function() {
+    //open modals
     const settingsButton = document.getElementById("settingsButton");
     const statsButton = document.getElementById("statsButton");
     const howtoButton = document.getElementById("howtoButton");
@@ -12,6 +13,18 @@ $(document).ready(function() {
     });
     howtoButton.addEventListener('click', () => {
         $('#howtoModal').modal('show');
+    });
+
+    //toggle dark mode
+    $('#darktheme').change(function() {
+        if(this.checked) {
+            $('#bd').addClass('night');
+            $('i').css("color", "white");
+        }
+        else {
+            $('#bd').removeClass('night');
+            $('i').css('color', 'black');
+        }
     });
 });
 
