3b8036b9608a14d3d15edf4b1ef038cbb0401f35
Jennifer Nguyen <thitrangngannguyen@gmail.com>
2022-05-22 16:35:48 +0800

    Stats Modal

--- app/static/interactive.js
+++ app/static/interactive.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     const settingsButton = document.getElementById("settingsButton");
+    const statsButton = document.getElementById("statsButton");
     settingsButton.addEventListener('click', () => {
         $('#settingsModal').modal('show');
-    })
+    });
+    statsButton.addEventListener('click', () => {
+        $('#statsModal').modal('show');
+    });
 });
 
